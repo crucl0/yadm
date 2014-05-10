d1a5e0a403c1df7a9f6df2452917fd90380c478e
crucl0 <alekz.polyakov@gmail.com>
2014-05-10 12:36:06 +0300

    The test of inheritance which doesn't pass

---
+++ tests/test_documents_inherit.py
@@ -0,0 +1,25 @@
+from unittest import TestCase
+
+from yadm.documents import Document
+from yadm import fields
+
+
+class DocumentsTest(TestCase):
+
+    def setUp(self):
+        class TestDoc(Document):
+            i = fields.IntegerField
+            b = fields.SetField(())
+
+        self.TestDoc = TestDoc
+
+    def test__db(self):
+        td = self.TestDoc()
+        self.assertIs(td.__db__, None)
+
+    def test_fields(self):
+        self.assertEqual(set(self.TestDoc.__fields__), {'_id', 'i', 'b'})
+
+    def test_inheritance_fields(self):
+        class InhTestDoc(self.TestDoc):
+            pass
